9d0188977412fc2689a43d502946ea3323edb03c
Mykhailo <mykhailotarabtsov@gmail.com>
2018-06-13 13:54:09 +0300

    maybe final version

--- js/main.js
+++ js/main.js
@@ -30,46 +30,84 @@ $(document).ready(function() {
 		// options
 		itemSelector: '.grid-item',
 		columnWidth: '.grid-sizer',
-  	percentPosition: true
+		// percentPosition: true,
+		// gutter: 20
 	});
 
 	// AJAX page load
 
 	// load index page when the page loads
-	$("#ajax-content").hide().load("home.html").fadeIn('slow');
+	$("#ajax-content").hide().load("home.html").fadeIn(600);
 	$("#home").click(function () {
 		// load home page on click
-		$("#ajax-content").hide().load("home.html").fadeIn('slow');
+		$("#ajax-content").hide().load("home.html").fadeIn(600);
 		api.close();
 		$('.hamburger-inner').removeClass('hamburger-reverse');
 	});
 	$("#projects").click(function () {
 			// load home page on click
-			$("#ajax-content").hide().load("projects.html").fadeIn('slow');
+			$("#ajax-content").hide().load("projects.html").fadeIn(600);
 			api.close();
 			$('.hamburger-inner').addClass('hamburger-reverse');
 	});
 	$("#about").click(function () {
 			// load about page on click
-			$("#ajax-content").hide().load("about.html").fadeIn('slow');
+			$("#ajax-content").hide().load("about.html").fadeIn(600);
 			api.close();
 			$('.hamburger-inner').addClass('hamburger-reverse');
 	});
 	$("#contact").click(function () {
 			// load contact form onclick
-			$("#ajax-content").hide().html("<h1>Page 3</h1><p>Page 3 has been loaded</p>").fadeIn('slow');
+			$("#ajax-content").hide().load("contact.html").fadeIn(600);
 			api.close();
+			$('.hamburger-inner').addClass('hamburger-reverse');
 	});
 	$("#about-project").click(function () {
 			// load contact form onclick
-			$("#ajax-content").hide().load("about-project.html").fadeIn('slow');
+			$("#ajax-content").hide().load("about-project.html").fadeIn(600);
 	});
 	$("#album").click(function () {
 		// load home page on click
-		$("#ajax-content").hide().load("album.html").fadeIn('slow');
+		$("#ajax-content").hide().load("album.html").fadeIn(600);
 		api.close();
 		$('.hamburger-inner').addClass('hamburger-reverse');
 	});
 
+	setTimeout(function() {
+		$('.my__preloader').css('display', 'none');
+	}, 1300)
 	
+	
+});
+var currentSlideMy = 0;
+$(document).ajaxComplete(function() {
+	
+	$(".album__buttonSlide").click(function (event) {
+		currentSlideMy = +(event.target.getAttribute('data-slideNumber') - 1);
+		console.log(currentSlideMy);
+		$("#ajax-content").hide().load("slide.html").fadeIn(600);
+	});
+	console.log(currentSlideMy);
+
+	$(".slide__project-slide").on("init", function(event, slick){
+		$(".count").text(parseInt(slick.currentSlide + 1) + ' / ' + slick.slideCount);
+	});
+
+	$(".slide__project-slide").on("afterChange", function(event, slick, currentSlide){
+		$(".count").text(parseInt(slick.currentSlide + 1) + ' / ' + slick.slideCount);
+	});
+
+	$('.slide__project-slide').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		adaptiveHeight: true,
+		prevArrow: '.my-prev__arrow',
+		nextArrow: '.my-next__arrow',
+		initialSlide: currentSlideMy
+	});
+	$("#about-project").click(function () {
+		// load contact form onclick
+		$("#ajax-content").hide().load("about-project.html").fadeIn(600);
+	});
 });
